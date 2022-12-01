8381fce2c6e6bbf511471b9ca370f94aca6ade23
Michael Elfassy <mike.elfassy@gmail.com>
2022-11-30 22:47:27 -0500

    commit tree 6488360d182a5c8496eb29fceffd81aa9b3e9423

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+// const superbowlWin = superbowl => superbowl.result === 'W'
+// console.log(record.find(superbowl => superbowl.result === 'W'))
+
+const superbowlWin = function(record) {
+    let result = record.find(record => record.result === 'W')
+    if (typeof result === "undefined") {return undefined}
+    else {return result.year}
+}
